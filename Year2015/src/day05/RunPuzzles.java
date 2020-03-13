2859a0e2a59bad5974f123053aee12a75ba93d27
Robyn Dunstan <dunstanr@members1st.org>
2020-03-13 16:47:17 -0400

    Start day 5

--- Year2015/src/day05/RunPuzzles.java
+++ Year2015/src/day05/RunPuzzles.java
@@ -1,4 +1,4 @@
-package day04;
+package day05;
 
 import java.util.ArrayList;
 
@@ -8,31 +8,29 @@ public class RunPuzzles {
 
 	public static void main(String[] args) {
 		ArrayList<TestCase<String, Integer>> testCases = createTestCases();
-		System.out.println("Day 04: The Ideal Stocking Stuffer\r\tSection 1");
+		System.out.println("Day 05: Doesn't He Have Intern-Elves For This?\r\tSection 1");
 		int testCaseCount = 0;
 		for (TestCase<String, Integer> test : testCases) {
 			if (test.section == 1) {
 				testCaseCount++;
-				System.out.println("\t\tTest " + testCaseCount + "\tExpected: " + test.result + "\tActual: " + TheIdealStockingStuffer.getFiveInitialZeros(test.input));
+				System.out.println("\t\tTest " + testCaseCount + "\tExpected: " + test.result + "\tActual: " + (test.input));
 			}
 		}
-		System.out.println("\t\tPuzzle: " + TheIdealStockingStuffer.getFiveInitialZeros(puzzleInput));
+		System.out.println("\t\tPuzzle: " + (puzzleInput));
 		
 		System.out.println("\r\tSection 2");
 		testCaseCount = 0;
 		for (TestCase<String, Integer> test : testCases) {
 			if (test.section == 2) {
 				testCaseCount++;
-				System.out.println("\t\tTest " + testCaseCount + "\tExpected: " + test.result + "\tActual: " + TheIdealStockingStuffer.getSixInitialZeros(test.input));
+				System.out.println("\t\tTest " + testCaseCount + "\tExpected: " + test.result + "\tActual: " + (test.input));
 			}
 		}
-		System.out.println("\t\tPuzzle: " + TheIdealStockingStuffer.getSixInitialZeros(puzzleInput));
+		System.out.println("\t\tPuzzle: " + (puzzleInput));
 	}
 
 	private static ArrayList<TestCase<String, Integer>> createTestCases() {
 		ArrayList<TestCase<String, Integer>> tests = new ArrayList<TestCase<String, Integer>>();
-		tests.add(new TestCase<String, Integer>(1, "abcdef", 609043));
-		tests.add(new TestCase<String, Integer>(1, "pqrstuv", 1048970));
 		return tests;
 	}
 
