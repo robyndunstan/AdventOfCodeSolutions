ae6dc4c311bdd4ec3a58ff24a6597ec0a577f8a4
Robyn Dunstan <DunstanR@members1st.org>
2022-12-08 16:53:42 -0500

    start designing filesystem class

--- src/Year2022/day07/NoSpaceLeftOnDevice.java
+++ src/Year2022/day07/NoSpaceLeftOnDevice.java
@@ -6,6 +6,8 @@ import tools.RunPuzzle;
 import tools.TestCase;
 
 public class NoSpaceLeftOnDevice extends RunPuzzle {
+	
+	private File root;
 
 	public NoSpaceLeftOnDevice(int dayNumber, String dayTitle, Object puzzleInput) {
 		super(dayNumber, dayTitle, puzzleInput);
@@ -32,6 +34,56 @@ public class NoSpaceLeftOnDevice extends RunPuzzle {
 	public static void main(String[] args) {
 		RunPuzzle puzzle = new NoSpaceLeftOnDevice(7, "No Space Left On Device", puzzleFile);
 		puzzle.run();
+	}
+	
+	void parseFileSystem(String filename) {
+		
+	}
+	
+	private static class File {
+		boolean isFolder;
+		String name;
+		ArrayList<File> contents;
+		private long size;
+		
+		public File() {
+			contents = new ArrayList<File>();
+		}
+		
+		public long getSize() {
+			if (isFolder) {
+				long totalSize = 0l;
+				for (File f : contents) {
+					totalSize += f.getSize();
+				}
+				return totalSize;
+			}
+			else {
+				return size;
+			}
+		}
+		public void setSize(long size) {
+			if (!isFolder) this.size = size;
+		}
+		
+		public File findFile(String path) {
+			if (path.charAt(0) == '/') {
+				path = path.substring(1);
+			}
+			
+			if (!isFolder) {
+				return name.equals(path) ? this : null;
+			}
+			
+			int slashIndex = path.indexOf("/");
+			String searchName = slashIndex > -1 ? path.substring(0, slashIndex) : path;
+			for (File f : contents) {
+				if (f.name.equals(searchName)) {
+					return slashIndex > -1 ? f.findFile(path.substring(slashIndex)) : f;
+				}
+			}
+			return null;
+		}
 	}
 
 	static String test1File = "src\\Year2022\\day07\\data\\test1File";
