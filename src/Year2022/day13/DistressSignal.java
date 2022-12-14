991adf170abae27d4a2edc5a5b3b4ca7799f4c23
Robyn Dunstan <DunstanR@members1st.org>
2022-12-14 15:36:20 -0500

    start coding processing day 13

--- src/Year2022/day13/DistressSignal.java
+++ src/Year2022/day13/DistressSignal.java
@@ -1,5 +1,9 @@
 package Year2022.day13;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import tools.RunPuzzle;
@@ -25,13 +29,61 @@ public class DistressSignal extends RunPuzzle {
 
 	@Override
 	public Object doProcessing(int section, Object input) {
-		// TODO Auto-generated method stub
-		return null;
+		String filename = (String)input;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			String packet1 = br.readLine();
+			String packet2 = br.readLine();
+			int packetIndex = 1;
+			do {
+				
+				br.readLine(); // blank line
+				packet1 = br.readLine();
+				packet2 = br.readLine();
+				packetIndex++;
+			} while (packet1 != null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
 		RunPuzzle puzzle = new DistressSignal(13, "Distress Signal", puzzleFile);
 		puzzle.run();
+	}
+	
+	public boolean isPacketOrderCorrect(String p1, String p2) {
+		return false;
+	}
+	
+	static class IntegerList {
+		int value;
+		boolean hasValue;
+		ArrayList<IntegerList> contents;
+		
+		public String parseItem(String item) {
+			if (item.charAt(0) == '[') {
+				item = item.substring(1);
+				hasValue = false;
+				contents = new ArrayList<IntegerList>();
+				
+				while (item.length() > 0 && item.charAt(0) != ']') {
+					IntegerList newItem = new IntegerList();
+					item = newItem.parseItem(item);
+					contents.add(newItem);
+				}
+				
+				if (item.charAt(0) == ']') item = item.substring(1);
+			}
+			else if (Character.isDigit(item.charAt(0))) {
+				String numberString = "";
+				// parse number
+			}
+			
+			if (item.charAt(0) == ',') item = item.substring(1);
+			return item;
+		}
 	}
 
 	static String test1File = "src\\Year2022\\day13\\data\\test1File";
