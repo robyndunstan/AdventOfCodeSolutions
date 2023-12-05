a95e650da78b2fdaca8f9984debf2ad28077689b
Robyn Dunstan <DunstanR@members1st.org>
2023-12-05 09:30:01 -0500

    empty day 1 class

---
+++ src/Year2023/day01/Trebuchet.java
@@ -0,0 +1,38 @@
+package Year2023.day01;
+
+import java.util.ArrayList;
+
+import tools.RunPuzzle;
+import tools.TestCase;
+
+public class Trebuchet extends RunPuzzle {
+
+	public Trebuchet(int dayNumber, String dayTitle, Object puzzleInput) {
+		super(dayNumber, dayTitle, puzzleInput);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ArrayList<TestCase> createTestCases() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printResult(Object result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object doProcessing(int section, Object input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
