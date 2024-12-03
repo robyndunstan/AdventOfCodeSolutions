12cc5e2988718fa957e808c0c72206d06a964ccf
Dunstan <DunstanR@members1st.org>
2024-12-03 16:50:56 -0500

    start filling in day 1

---
+++ src/Year2024/day01/HistorianHysteria.java
@@ -0,0 +1,37 @@
+package Year2024.day01;
+
+import java.util.ArrayList;
+
+import tools.TestCase;
+
+public class HistorianHysteria extends tools.RunPuzzle {
+
+    public HistorianHysteria(int dayNumber, String dayTitle, Object puzzleInput) {
+        super(dayNumber, dayTitle, puzzleInput);
+        debug = false;
+    }
+
+    public static void main(String[] args) {
+        RunPuzzle rp = new HistorianHysteria(1, "Historian Hysteria", "");
+        rp.run();
+    }
+
+    @Override
+    public ArrayList<TestCase> createTestCases() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createTestCases'");
+    }
+
+    @Override
+    public void printResult(Object result) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printResult'");
+    }
+
+    @Override
+    public Object doProcessing(int section, Object input) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doProcessing'");
+    }
+    
+}
