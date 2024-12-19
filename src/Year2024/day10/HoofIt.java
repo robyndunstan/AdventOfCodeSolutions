969bc61dbb1b065f963558dc96a1803770fa7985
robyndunstan <dunstanrm@gmail.com>
2024-12-19 11:10:52 -0500

    day 10 section 2 complete

--- src/Year2024/day10/HoofIt.java
+++ src/Year2024/day10/HoofIt.java
@@ -1,7 +1,9 @@
 package Year2024.day10;
 
+import java.awt.Point;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import tools.FileController;
 import tools.RunPuzzle;
 import tools.TestCase;
@@ -15,8 +17,8 @@ public class HoofIt extends tools.RunPuzzle {
     }
 
     public static void main(String[] args) throws IOException {
-        RunPuzzle p = new HoofIt(10, "Hoof It", "src\\Year2024\\day010\\data\\puzzleFile");
-        //p.setLogFile("src\\Year2024\\day10\\data\\log.txt");
+        RunPuzzle p = new HoofIt(10, "Hoof It", "src\\Year2024\\day10\\data\\puzzleFile");
+        p.setLogFile("src\\Year2024\\day10\\data\\log.txt");
         p.run();
     }
 
@@ -28,6 +30,10 @@ public class HoofIt extends tools.RunPuzzle {
         tests.add(new TestCase<>(1, "src\\Year2024\\day10\\data\\test3File", 4));
         tests.add(new TestCase<>(1, "src\\Year2024\\day10\\data\\test4File", 3));
         tests.add(new TestCase<>(1, "src\\Year2024\\day10\\data\\test5File", 36));
+        tests.add(new TestCase<>(2, "src\\Year2024\\day10\\data\\test6File", 3));
+        tests.add(new TestCase<>(2, "src\\Year2024\\day10\\data\\test7File", 13));
+        tests.add(new TestCase<>(2, "src\\Year2024\\day10\\data\\test8File", 227));
+        tests.add(new TestCase<>(2, "src\\Year2024\\day10\\data\\test5File", 81));
         return tests;
     }
 
@@ -81,13 +87,84 @@ public class HoofIt extends tools.RunPuzzle {
                 file.closeFile();
             } catch (IOException ex) {}
         }
+        logDebug("Map " + elevationMap.get(0).size() + " by " + elevationMap.size());
 
-        if (section == 1) {
-            return null;
+        HashMap<Point, ArrayList<Point>> paths = new HashMap<>();
+        for (int y = 0; y < elevationMap.size(); y++) {
+            ArrayList<Integer> row = elevationMap.get(y);
+            for (int x = 0; x < row.size(); x++) {
+                if (row.get(x) == 0) {
+                    ArrayList<Point> currentPoints = new ArrayList<>();
+                    currentPoints.add(new Point(x, y));
+                    paths.put(new Point(x, y), currentPoints);
+                }
+            }
         }
-        else {
-            return null;
+        logDebug("Found " + paths.keySet().size() + " starting points");
+
+        for (int targetElevation = 1; targetElevation <= 9; targetElevation++) {
+            logDebug("Search for elevation " + targetElevation);
+            for (Point pathStart : paths.keySet()) {
+                ArrayList<Point> nextPoints = new ArrayList<>();
+                ArrayList<Point> currentPoints = paths.get(pathStart);
+                for (Point c : currentPoints) {
+                    logDebug("Testing point (" + c.x + ", " + c.y + ")");
+                    Point east = new Point(c.x + 1, c.y);
+                    logDebug("East " + getElevation(east));
+                    if (getElevation(east) == targetElevation) nextPoints.add(east);
+                    Point north = new Point(c.x, c.y - 1);
+                    logDebug("North " + getElevation(north));
+                    if (getElevation(north) == targetElevation) nextPoints.add(north);
+                    Point west = new Point(c.x - 1, c.y);
+                    logDebug ("West " + getElevation(west));
+                    if (getElevation(west) == targetElevation) nextPoints.add(west);
+                    Point south = new Point(c.x, c.y + 1);
+                    logDebug("South " + getElevation(south));
+                    if (getElevation(south) == targetElevation) nextPoints.add(south);
+                }
+                if (section == 1) {
+                    logDebug("Before dedup " + nextPoints.size());
+                    ArrayList<Point> dedup = new ArrayList<>();
+                    for (int i = 0; i < nextPoints.size() - 1; i++) {
+                        boolean matchFound = false;
+                        Point pi = nextPoints.get(i);
+                        for (int j = i + 1; j < nextPoints.size(); j++) {
+                            Point pj = nextPoints.get(j);
+                            if (pi.x == pj.x && pi.y == pj.y) {
+                                matchFound = true;
+                                break;
+                            }
+                        }
+                        if (!matchFound) dedup.add(pi);
+                    }
+                    dedup.add(nextPoints.get(nextPoints.size() - 1));
+                    if (debug) {
+                        for (Point p : dedup) {
+                            logDebug("\t(" + p.x + ", " + p.y + ")");
+                        }
+                    }
+                    paths.put(pathStart, dedup);
+                }
+                else {
+                    paths.put(pathStart, nextPoints);
+                }
+                logDebug("Start (" + pathStart.x + ", " + pathStart.y + ") has " + paths.get(pathStart).size() + " points at elevation " + targetElevation);
+            }
         }
+
+        int sumPaths = 0;
+        for (Point pathStart : paths.keySet()) {
+            sumPaths += paths.get(pathStart).size();
+        }
+        return sumPaths;
     }
 
+    private int getElevation(Point p) {
+        if (p.y >= 0 && p.y < elevationMap.size() && p.x >= 0 && p.x < elevationMap.get(p.y).size()) {
+            return elevationMap.get(p.y).get(p.x);
+        }
+        else {
+            return -1;
+        }
+    }
 }
