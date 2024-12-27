2dfbcccfbd6e625a9831ed19a99a7820a6f0e20a
robyndunstan <dunstanrm@gmail.com>
2024-12-27 16:35:50 -0500

    day 13 section 1 pass, section 2 fail

--- src/Year2024/day13/ClawContraption.java
+++ src/Year2024/day13/ClawContraption.java
@@ -1,8 +1,8 @@
 package Year2024.day13;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.ArrayList;
-
 import tools.FileController;
 import tools.RunPuzzle;
 import tools.TestCase;
@@ -14,8 +14,8 @@ public class ClawContraption extends tools.RunPuzzle {
     }
 
     public static void main(String[] args) {
-        RunPuzzle p = new ClawContraption(13, "Claw Contraption", "src\\Year2024\\day13\\data\\test1file");
-        //RunPuzzle p = new ClawContraption(13, "Claw Contraption", "src\\Year2024\\day13\\data\\puzzleFile");
+        //RunPuzzle p = new ClawContraption(13, "Claw Contraption", "src\\Year2024\\day13\\data\\test1file");
+        RunPuzzle p = new ClawContraption(13, "Claw Contraption", "src\\Year2024\\day13\\data\\puzzleFile");
         //p.setLogFile("src\\Year2024\\day13\\data\\log.txt");
         p.run();
     }
@@ -23,63 +23,69 @@ public class ClawContraption extends tools.RunPuzzle {
     @Override
     public ArrayList<TestCase> createTestCases() {
         ArrayList<TestCase> tests = new ArrayList<>();
-        tests.add(new TestCase<>(1, "src\\Year2024\\day13\\data\\test1File", 480));
+        tests.add(new TestCase<>(1, "src\\Year2024\\day13\\data\\test1File", new BigInteger("480")));
+        tests.add(new TestCase<>(2, "src\\Year2024\\day13\\data\\test1File", new BigInteger("875318608908")));
         return tests;
     }
 
     @Override
     public void printResult(Object result) {
-        log(defaultOutputIndent + (Integer)result);
+        log(defaultOutputIndent + (BigInteger)result);
     }
 
     @Override
-    public Object doProcessing(int section, Object input) {
+    public Object doProcessing(int section, Object input) { // 94196527538398 too high, 31065 too low
         String fileName = (String)input;
         FileController file = new FileController(fileName);
 
         try {
             file.openInput();
             String line = file.readLine();
-            int tokens = 0;
-            Point a, b, prize;
-            int aTokens = 3;
-            int bTokens = 1;
+            BigInteger tokens = new BigInteger("0");
+            BigIntPoint a = new BigIntPoint(), b = new BigIntPoint(), prize = new BigIntPoint();
+            BigInteger aTokens = new BigInteger("3");
+            BigInteger bTokens = new BigInteger("1");
 
             while (line != null) {
                 line = line.trim();
                 if (line.startsWith("Button A")) {
                     int i1 = line.indexOf("X+");
                     int i2 = line.indexOf(",");
-                    a.x = Integer.valueOf(line.substring(i1 + 2, i2));
+                    a.x = new BigInteger(line.substring(i1 + 2, i2));
                     i1 = line.indexOf("Y+");
-                    a.y = Integer.valueOf(line.substring(i1 + 2));
+                    a.y = new BigInteger(line.substring(i1 + 2));
                 }
                 else if (line.startsWith("Button B")) {
                     int i1 = line.indexOf("X+");
                     int i2 = line.indexOf(",");
-                    b.x = Integer.valueOf(line.substring(i1 + 2, i2));
+                    b.x = new BigInteger(line.substring(i1 + 2, i2));
                     i1 = line.indexOf("Y+");
-                    b.y = Integer.valueOf(line.substring(i1 + 2));
+                    b.y = new BigInteger(line.substring(i1 + 2));
                 } else if (line.startsWith("Prize")) {
                     int i1 = line.indexOf("X=");
                     int i2 = line.indexOf(",");
-                    prize.x = Integer.valueOf(line.substring(i1 + 2, i2));
+                    prize.x = new BigInteger(line.substring(i1 + 2, i2));
                     i1 = line.indexOf("Y=");
-                    prize.y = Integer.valueOf(line.substring(i1 + 2));
+                    prize.y = new BigInteger(line.substring(i1 + 2));
+
+                    if (section == 2) {
+                        prize.x = prize.x.add(new BigInteger("10000000000000"));
+                        prize.y = prize.y.add(new BigInteger("10000000000000"));
+                    }
 
                     // calculate the number of tokens
-                    int bTop = a.y * prize.x - a.x * prize.y;
-                    int bBottom = a.y * b.x - a.x * b.y;
-                    if (bTop % bBottom == 0) {
-                        int bCount = bTop / bBottom;
-                        int aCount = (prize.x - b.x * bCount) / a.x;
-                        tokens += aTokens * aCount + bTokens * bCount;
+                    BigInteger bTop = a.y.multiply(prize.x).subtract(a.x.multiply(prize.y));
+                    BigInteger bBottom = a.y.multiply(b.x).subtract(a.x.multiply(b.y));
+                    if (bTop.remainder(bBottom).equals(new BigInteger("0"))) {
+                        BigInteger bCount = bTop.divide(bBottom);
+                        BigInteger aCount = prize.x.subtract(b.x.multiply(bCount)).divide(a.x);
+                        tokens = tokens.add(aTokens.multiply(aCount)).add(bTokens.multiply(bCount));
                     }
                 }
                 else {
-                    a = new Point();
-                    b = new Point();
-                    prize = new Point();
+                    a = new BigIntPoint();
+                    b = new BigIntPoint();
+                    prize = new BigIntPoint();
                 }
                 line = file.readLine();
             }
@@ -88,6 +94,15 @@ public class ClawContraption extends tools.RunPuzzle {
         catch (IOException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    private class BigIntPoint {
+        public BigInteger x;
+        public BigInteger y;
+        public BigIntPoint() {
+            x = new BigInteger("0");
+            y = new BigInteger("0");
         }
     }
 }
